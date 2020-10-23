a2522d1fd35687e3b8163052a5c0092bf7fed9a8
Craft <craftworx@protonmail.com>
2020-10-24 01:10:25 +0200

    essai de commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 
 # structure minimale, on installé les packages et créé une app, ni widget ni layout
+# elle tourne mais n'a pas de bouton ni de focntion
 
 class LelabApp(App):
     pass
