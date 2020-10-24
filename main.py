f47e7e41cd2d7ca29be60c1166d5ed7a4b594fc0
Craft <craftworx@protonmail.com>
2020-10-24 10:09:16 +0200

    avec fichier de configuration kivy

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
 
 # structure minimale, on installé les packages et créé une app, ni widget ni layout
 # elle tourne mais n'a pas de bouton ni de focntion
+class MainWidget(Widget):
+    pass
 
 class LelabApp(App):
     pass
